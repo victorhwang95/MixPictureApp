70a3f669a41b4561e4239314ea9ac5a129e26571
Le Hoang Vinh <VinhLH@minerva.vn>
2019-06-21 20:20:24 +0700

    Initial Commit

---
+++ MixPictureApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MixPictureApp
+//
+//  Created by Lê Hoàng Vinh on 6/21/19.
+//  Copyright © 2019 Lê Hoàng Vinh. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
